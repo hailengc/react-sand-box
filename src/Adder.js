8a72e093927856452957c941c8f0e50c8e327b5b
Hailong Cao <hailengc@gmail.com>
2018-11-16 16:21:01 +0800

    Initial commit

---
+++ src/Adder.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+class Adder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: props.init
+    };
+  }
+
+  onAdd() {
+    this.setState((s, p) => {
+      return { result: s.result + p.increment };
+    });
+  }
+
+  onReset() {
+    this.setState({
+      result: this.props.init
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log("... will receive props called..");
+    console.log(this.props);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log(".... willUpdate called..");
+    console.log(this.props, this.state);
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    console.log("....didupdate called")    
+    console.log(prevProps, prevState)
+  }
+
+  render() {
+    return (
+      <div style={{ border: "1px solid blue" }}>
+        <div>{this.state.result}</div>
+        <button onClick={this.onAdd.bind(this)}>Add</button>
+        <button onClick={this.onReset.bind(this)}>Reset</button>
+      </div>
+    );
+  }
+}
+
+export default Adder;
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Adder from "./Adder";
+import "./styles.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      init: 1000,
+      increment: 5
+    };
+  }
+
+  onInitChange(e) {
+    this.setState({ init: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="number"
+          value={this.state.init}
+          onChange={this.onInitChange.bind(this)}
+        />
+        <Adder {...this.state} />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
